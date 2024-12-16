9e696f7263d424c8a03b101c28e018bab4407ec4
alkrivosheev <alkrivosheev@gmail.com>
2024-12-16 14:21:40 +0300

    ConsoleProgress

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+            while (!Thread.currentThread().isInterrupted()) {
+                for (char c : process) {
+                    System.out.print("\r load: " + c);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+            }
+        }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
